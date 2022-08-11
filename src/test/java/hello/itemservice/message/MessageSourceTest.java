739876e44b81c83a208302d1d647e34c826da321
nayeon-jo <nyc0516@naver.com>
2022-08-11 12:58:56 +0900

    스프링 메시지 소스 사용

--- src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -19,6 +19,8 @@ public class MessageSourceTest {
     @Test
     void helloMessage() {
         String result = ms.getMessage("hello", null, null);
+        //locale 정보가 없으면 basename에서 설정한 기본 이름 메시지 파일을 조회
+        //basename 으로 messages를 지정 했으므로 messages.properties 파일에서 데이터 조회
         assertThat(result).isEqualTo("안녕");
     }
 
@@ -26,27 +28,35 @@ public class MessageSourceTest {
     void notFoundMessageCode() {
         assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
                 .isInstanceOf(NoSuchMessageException.class);
+        //메시지가 없는 경우에는 NoSuchMessageException이 발생
     }
     @Test
     void notFoundMessageCodeDefaultMessage() {
         String result = ms.getMessage("no_code", null, "기본 메시지", null);
         assertThat(result).isEqualTo("기본 메시지");
+        //메시지가 없어도 기본 메시지(defaultMessage)를 사용하면 기본 메시지가 반환
     }
 
     @Test
     void argumentMessage() {
         String result = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
         assertThat(result).isEqualTo("안녕 Spring");
+        //hello.name=안녕 {0} -> {0} 부분은 매개변수(args: Spring) 전달 ->안녕 Spring
     }
 
     @Test
     void defaultLang() {
         assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
+        //locale 정보가 없으므로 messages 를 사용
+
         assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+        //locale 정보가 있지만, message_ko 가 없으므로 messages 를 사용
     }
 
     @Test
     void enLang() {
+
         assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+        // locale 정보가 Locale.ENGLISH 이므로 messages_en 을 찾아서 사용
     }
 }
